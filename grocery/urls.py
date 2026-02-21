34d21f246305826718bcb32c4c1300b0c14c3daa
kiranP <keyrun602@gmail.com>
2026-02-21 09:18:26 +0545

    fixed final bugs.

--- grocery/urls.py
+++ grocery/urls.py
@@ -1,8 +1,9 @@
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('delete/<int:id>/', views.delete_item),
-    path('toggle/<int:id>/', views.toggle_complete),
+    path('', views.index, name='index'),
+    path('delete/<int:id>/', views.delete_item, name='delete'),
+    path('toggle/<int:id>/', views.toggle_complete, name='toggle'),
 ]
